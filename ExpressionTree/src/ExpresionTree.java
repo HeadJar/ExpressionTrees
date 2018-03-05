4d2507c86513e0db10750a70e12be67c314f55c2
"HeadJar" <"jarhead4123@gmail.com">
2018-03-04 22:50:26 -0500

    asdf

--- ExpressionTree/src/ExpresionTree.java
+++ ExpressionTree/src/ExpresionTree.java
@@ -1,4 +1,54 @@
+import javax.swing.tree.TreeNode;
 
-public class ExpresionTree {
+public class ExpresionTree implements Expressions{
+
+	@Override
+	public TreeNode buildTree(String[] exp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int evalTree(TreeNode root) {
+	if (root != null) {
+		if(!isOperator(root)) {
+
+			return Integer.parseInt(root.toString());
+		}
+		evalTree(root.getLeft);
+		evalTree(root.getRight);
+	}
+		return 0;
+	}
+	
+	public boolean isOperator(TreeNode root) {
+		if(root.toString().equals("+") || root.toString().equals("*")) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String toPrefixNotation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toInfixNotation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toPostfixNotation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int postfixEval(String[] exp) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
